8826f7fcf83bd1cff99015a657856689dbccb0fb
Connor-Walden <connorwalden1403@gmail.com>
2021-05-22 14:48:39 +0930

    changes chat size in chatbox

--- client/src/components/RecievedChat/RecievedChat.js
+++ client/src/components/RecievedChat/RecievedChat.js
@@ -2,7 +2,7 @@ function RecievedChat({ theme, recieverData, message }) {
     return (
         <li className="chat-right" key={message.id}>
             <div className={theme=='dark' ? "chat-hour text-light" : "chat-hour text-dark"}>{message.sent_on} <span className="fa fa-check-circle"></span></div>
-            <img className={theme == 'dark' ? "chat-text bg-secondary" : "chat-text bg-dark"} width="350px" src={message.message} style={{ borderRadius: '0', width: '150px', height: '150px' }} />
+            <img className={theme == 'dark' ? "chat-text bg-secondary" : "chat-text bg-dark"} width="350px" src={message.message} style={{ borderRadius: '0', width: '250px', height: '150px' }} />
             <div className="chat-avatar">
                 <img src={recieverData.profile.profile_picture} alt="Reciever profile picture" />
                 <div className="chat-name">{recieverData.username}</div>
